ff13f40a0c6de0c39454686e019e82053cb6e5b5
Carlos Montes <carlos.montes@keysight.com>
2024-07-15 14:22:43 -0700

    Updating Test Plan Generator project setting Standard Channel children visible to show as an expanded group in Editor

--- TestPlanGenerator/TestGenModel.cs
+++ TestPlanGenerator/TestGenModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Office.Interop.Excel;
 using OpenTap;
 using OpenTap.Plugins.PNAX;
+using OpenTap.Plugins.BasicSteps;
 
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static OpenTap.Plugins.PNAX.PNAX;
+using System.IO;
 
 namespace TestPlanGenerator
 {
@@ -43,6 +45,19 @@ namespace TestPlanGenerator
         public double Power { get; set; }
         public double CW { get; set; }
     }
+
+    public class ExcelDialogStep
+    {
+        public String StepType { get; set; }
+        public String MessageID { get; set; }
+        public String Message { get; set; }
+        public InputButtons DialogButtons { get; set; }
+        public bool ShowPicture { get; set; }
+        public String URL { get; set; }
+        public Verdict PositiveVerdict { get; set; }
+        public Verdict NegativeVerdict { get; set; }
+    }
+
 
     public class StandardPhaseSweep : StandardSweep
     {
@@ -79,6 +94,20 @@ namespace TestPlanGenerator
             set
             {
                 _TestPlanInputFileName = value;
+                RaisePropertyChanged("TestPlanInputFileName");
+            }
+        }
+
+        private String _TestPlanDialogInputFileName;
+        public String TestPlanDialogInputFileName
+        {
+            get
+            {
+                return _TestPlanDialogInputFileName;
+            }
+            set
+            {
+                _TestPlanDialogInputFileName = value;
                 RaisePropertyChanged("TestPlanInputFileName");
             }
         }
@@ -343,5 +372,222 @@ namespace TestPlanGenerator
 
             return theArray;
         }
+
+
+        public TestPlan GenerateDialogTestPlanCSV()
+        {
+            TestPlan testPlan = new TestPlan();
+            testPlan.ChildTestSteps.Clear();
+
+            this.traceSource.Info("Starting Test Sequence Generation from CSV");
+
+            this.traceSource.Info("TestPlanInputFileName: " + TestPlanDialogInputFileName.ToString());
+
+
+
+
+
+            return testPlan;
+        }
+
+        public TestPlan GenerateDialogTestPlan()
+        {
+            this.traceSource.Info("Starting Test Sequence Generation");
+
+            this.traceSource.Info("TestPlanInputFileName: " + TestPlanDialogInputFileName.ToString());
+            this.traceSource.Info("TestPlanOutputFileName: " + TestPlanOutputFileName.ToString());
+
+            TestPlan testPlan = new TestPlan();
+            testPlan.ChildTestSteps.Clear();
+            ListOfchannels = new List<object>();
+            ListOfTraces = new List<object>();
+            List<StandardTrace> standardTraces = new List<StandardTrace>();
+
+            if (TestPlanDialogInputFileName.EndsWith("xlsx"))
+            {
+                ParseDialogExcelFile(TestPlanDialogInputFileName);
+            }
+            else if (TestPlanDialogInputFileName.EndsWith("csv"))
+            {
+                // Open and Parse CSV file
+                ParseDialogCSV(TestPlanDialogInputFileName);
+            }
+            else
+            {
+                throw new Exception("Unsupported input file!");
+            }
+
+            // foreach Channel in csv file
+            // add channel to childsteps
+            foreach (object o in ListOfchannels)
+            {
+                ExcelDialogStep excelDialogStep = o as ExcelDialogStep;
+                if (excelDialogStep.StepType.Equals("Dialog"))
+                {
+                    OpenTap.Plugins.BasicSteps.DialogStep dialogStep = new DialogStep()
+                    {
+                        Title = excelDialogStep.MessageID,
+                        Message = excelDialogStep.Message,
+                        Buttons = excelDialogStep.DialogButtons,
+                        ShowPicture = excelDialogStep.ShowPicture,
+                        PictureSource = excelDialogStep.URL,
+                        Name = excelDialogStep.MessageID,
+                        PositiveAnswer = excelDialogStep.PositiveVerdict,
+                        NegativeAnswer = excelDialogStep.NegativeVerdict
+                    };
+                    testPlan.ChildTestSteps.Add(dialogStep);
+                }
+            }
+
+            return testPlan;
+        }
+
+        public void ParseDialogCSV(String filename)
+        {
+            int NumberOfHeaders = 0;
+            using (var reader = new StreamReader(TestPlanDialogInputFileName))
+            {
+                string line;
+                int rowNumber = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    String[] values = line.Split(',');
+
+                    rowNumber++;
+                    if (rowNumber == 1)
+                    {
+                        // headers
+                        NumberOfHeaders = values.Count();
+                        continue;
+                    }
+
+                    if (values[0].Equals("Dialog"))
+                    {
+                        if (values.Count() != NumberOfHeaders)
+                        {
+                            throw new Exception($"Row {rowNumber}: {line} \nhas an incorrect character, check the message does not contain any commas");
+                        }
+
+                        ExcelDialogStep dialogStep = new ExcelDialogStep();
+                        dialogStep.StepType = values[0];
+                        dialogStep.MessageID = values[1];
+                        dialogStep.Message = values[2];
+                        if (values[3].Equals("OK/Cancel"))
+                        {
+                            dialogStep.DialogButtons = InputButtons.OkCancel;
+                        }
+                        else
+                        {
+                            dialogStep.DialogButtons = InputButtons.YesNo;
+                        }
+                        dialogStep.ShowPicture = bool.Parse(values[4]);
+                        dialogStep.URL = values[5];
+                        if (values[6].Equals("Pass"))
+                        {
+                            dialogStep.PositiveVerdict = Verdict.Pass;
+                        }
+                        else if (values[6].Equals("Fail"))
+                        {
+                            dialogStep.PositiveVerdict = Verdict.Fail;
+                        }
+                        else
+                        {
+                            dialogStep.PositiveVerdict = Verdict.NotSet;
+                        }
+
+                        if (values[7].Equals("Pass"))
+                        {
+                            dialogStep.NegativeVerdict = Verdict.Pass;
+                        }
+                        else if (values[7].Equals("Fail"))
+                        {
+                            dialogStep.NegativeVerdict = Verdict.Fail;
+                        }
+                        else
+                        {
+                            dialogStep.NegativeVerdict = Verdict.NotSet;
+                        }
+                        ListOfchannels.Add(dialogStep);
+                    }
+
+                }
+            }
+        }
+
+        public void ParseDialogExcelFile(String filename)
+        {
+            this.traceSource.Info("Importing file: " + filename);
+            Workbook wb = null;
+            Application app = null;
+
+            app = new Application();
+            wb = app.Workbooks.Open(filename);
+
+            // Parse Channels
+            int sheetIndex = 1;
+            Worksheet sheet = (Worksheet)wb.Sheets.Item[sheetIndex];
+            Range excelRange = sheet.UsedRange;
+            foreach (Range row in excelRange.Rows)
+            {
+                int rowNumber = row.Row;
+
+                if (rowNumber == 1)
+                {
+                    continue;
+                }
+
+                int col = excelRange.Columns.Count;
+                string columnLetter = GetExcelColumnName(col);
+                string[] values = GetRange("A" + rowNumber + ":" + columnLetter + rowNumber + "", sheet);
+
+                if (values[0].Equals("Dialog"))
+                {
+                    ExcelDialogStep dialogStep = new ExcelDialogStep();
+                    dialogStep.StepType = values[0];
+                    dialogStep.MessageID = values[1];
+                    dialogStep.Message = values[2];
+                    if (values[3].Equals("OK/Cancel"))
+                    {
+                        dialogStep.DialogButtons = InputButtons.OkCancel;
+                    }
+                    else
+                    {
+                        dialogStep.DialogButtons = InputButtons.YesNo;
+                    }
+                    dialogStep.ShowPicture = bool.Parse(values[4]);
+                    dialogStep.URL = values[5];
+                    if (values[6].Equals("Pass"))
+                    {
+                        dialogStep.PositiveVerdict = Verdict.Pass;
+                    }
+                    else if (values[6].Equals("Fail"))
+                    {
+                        dialogStep.PositiveVerdict = Verdict.Fail;
+                    }
+                    else
+                    {
+                        dialogStep.PositiveVerdict = Verdict.NotSet;
+                    }
+
+                    if (values[7].Equals("Pass"))
+                    {
+                        dialogStep.NegativeVerdict = Verdict.Pass;
+                    }
+                    else if (values[7].Equals("Fail"))
+                    {
+                        dialogStep.NegativeVerdict = Verdict.Fail;
+                    }
+                    else
+                    {
+                        dialogStep.NegativeVerdict = Verdict.NotSet;
+                    }
+                    ListOfchannels.Add(dialogStep);
+                }
+            }
+            wb.Close();
+
+        }
+
+
     }
 }
